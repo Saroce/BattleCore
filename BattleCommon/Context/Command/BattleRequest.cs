9f74144530f4bfb835eea478e8ad017f3471611e
Saroce <Saroce233@163.com>
2023-03-24 19:28:04 +0800

    添加命令

---
+++ BattleCommon/Context/Command/BattleRequest.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------
+//        File:  BattleRequest.cs
+//       Brief:  BattleRequest
+//
+//      Author:  Saroce, Saroce233@163.com
+//
+//    Modified:  2023-03-24
+//============================================================
+
+using System;
+
+namespace Battle.Common.Context.Command
+{
+    public class BattleRequest : IBattleRequest
+    {
+        private readonly IBattleRespond _respond;
+
+        public BattleRequest(Type respond) {
+            _respond = Activator.CreateInstance(respond) as IBattleRespond;
+        }
+
+        public IBattleRespond GetRespond() => _respond;
+    }
+}
---
+++ BattleCommon/Context/Command/BattleRespond.cs
@@ -0,0 +1,41 @@
+﻿//------------------------------------------------------------
+//        File:  BattleRespond.cs
+//       Brief:  BattleRespond
+//
+//      Author:  Saroce, Saroce233@163.com
+//
+//    Modified:  2023-03-24
+//============================================================
+
+using System.Collections;
+
+namespace Battle.Common.Context.Command
+{
+    public class BattleRespond : IBattleRespond
+    {
+        private class YieldInstruction : IEnumerator
+        {
+            public bool KeepWaiting { get; set; } = true;
+
+            public bool MoveNext() => KeepWaiting;
+
+            public void Reset() {
+                KeepWaiting = false;
+            }
+
+            public object Current { get; } = null;
+        }
+
+        private readonly YieldInstruction _iter;
+
+        public BattleRespond() {
+            _iter = new YieldInstruction();
+        }
+        
+        public IEnumerator Receive() => _iter;
+
+        public bool Result { get; set; }
+        
+        public string ErrorMessage { get; set; }
+    }
+}
