3c11746d20270a75b6da7512c5461b28c80b540e
Saroce <Saroce233@163.com>
2023-04-01 12:28:48 +0800

    添加Avatar相关组件

---
+++ BattleView/Thing/Component/Avatar/AvatarAssetComponent.cs
@@ -0,0 +1,19 @@
+﻿//------------------------------------------------------------
+//        File:  AvatarAssetComponent.cs
+//       Brief:  AvatarAssetComponent
+//
+//      Author:  Saroce, Saroce233@163.com
+//
+//    Modified:  2023-03-31
+//============================================================
+
+using Battle.View.Base.CSExtension;
+
+namespace Battle.View.Thing.Component.Avatar
+{
+    [ViewThing]
+    public class AvatarAssetComponent : ViewComponent
+    {
+        public string Value;
+    }
+}
